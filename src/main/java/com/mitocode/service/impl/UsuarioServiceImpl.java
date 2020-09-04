eabf1d5b010ccdbbd035126282590435a1191731
root <root@debian>
2020-09-03 18:19:48 -0600

    primer commit

---
+++ src/main/java/com/mitocode/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,36 @@
+package com.mitocode.service.impl;
+
+import java.io.Serializable;
+
+import javax.ejb.EJB;
+import javax.inject.Named;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+import com.mitocode.dao.IUsuarioDAO;
+import com.mitocode.model.Usuario;
+import com.mitocode.service.IUsuarioService;
+
+@Named
+public class UsuarioServiceImpl implements IUsuarioService, Serializable {
+
+	@EJB
+	private IUsuarioDAO dao;
+
+	@Override
+	public Usuario login(Usuario us) {
+		String clave = us.getContrasena();
+		String claveHash = dao.traerPassHashed(us.getUsuario());
+
+		try {
+			if (BCrypt.checkpw(clave, claveHash)) {
+				return dao.leerPorNombreUsuario(us.getUsuario());
+			}
+		} catch (Exception e) {
+			throw e;
+		}
+
+		return new Usuario();
+	}
+
+}
